c7741c3b7e73ea8a48410810490784cb2d6be2a1
joice <290878726@qq.com>
2018-12-21 15:21:30 +0800

    详情页banner和放大轮播

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1809/9f/9f1a73fa9754fdeaa3.img.jpg_600x330_a8963c2a.jpg">
+      <div class="banner-info">
+        <div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+        <div class="banner-number">
+          <span class="iconfont icon-tupian banner-icon"></span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1809/65/65dd76c2a4d83de2a3.img.jpg_350x240_dc663b4a.jpg', 'http://img1.qunarzz.com/sight/p0/1809/65/65dd76c2a4d83de2a3.img.jpg_350x240_dc663b4a.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    height: 0
+    padding-bottom: 55%
+    overflow: hidden
+    .banner-img
+      width: 100%;
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      color: #fff
+      line-height: .6rem
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        margin-top: .14rem
+        padding: 0 .4rem
+        height: .4rem
+        font-size: .24rem
+        line-height: .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        .banner-icon
+          font-size: .24rem
+</style>
